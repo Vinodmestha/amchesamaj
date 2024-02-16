ad7a4a9b7f194dd27bf14a4340636bfa735cd82b
vinod_mestha <vinodmestha399@gmail.com>
2024-02-17 00:44:09 +0530

    project initialized

--- src/routes/MainRoutes/index.jsx
+++ src/routes/MainRoutes/index.jsx
@@ -3,5 +3,5 @@ import { useRoutes, Outlet } from "react-router-dom";
 
 export default function MainRoutes(props) {
     const routes = useRoutes([Outlet]);
-    return <div className="">dd{routes}</div>;
+    return <div className="">Amche Samaj{routes}</div>;
 }
