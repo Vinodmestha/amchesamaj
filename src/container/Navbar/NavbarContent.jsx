19d4ac1d42710d0252ae3345e8dcc7713e0ad23f
vinod_mestha <vinodmestha399@gmail.com>
2024-02-17 15:30:54 +0530

    header, hero responsive

--- src/container/Navbar/NavbarContent.jsx
+++ src/container/Navbar/NavbarContent.jsx
@@ -14,19 +14,19 @@ export default function NavbarContent(props) {
             <Container>
                 <div className="flex justify-between items-center py-6">
                     <div>
-                        <h1 className="text-green-100 text-3xl font-bold">Amche <span className="text-orange-500 font-mono">Samaj</span></h1>
+                        <h1 className="text-green-100 text-2xl sm:text-3xl font-bold">Amche <span className="text-orange-500 font-mono">Samaj</span></h1>
                         <p className="text-xs font-semibold text-green-300 text-right">
                             Mestha
                         </p>
                     </div>
-                    <div>
+                    <div className="hidden sm:block">
                         <ul className="flex gap-10">
                             {navLinks?.map((item, i) => (
                                 <li key={i} className="text-white text-lg">{item?.label}</li>
                             ))}
                         </ul>
                     </div>
-                    <div className="flex gap-3 items-center text-white text-lg ">
+                    <div className="hidden sm:flex gap-3 items-center text-white text-lg ">
                         <button>Login</button>/
                         <button>Sign Up</button>
                     </div>
--- src/routes/MainRoutes/Pages/Hero.jsx
+++ src/routes/MainRoutes/Pages/Hero.jsx
@@ -6,16 +6,16 @@ import Container from '../../../components/container'
 export default function Hero() {
     return (
         <Container>
-            <div className=' flex items-center pt-20'>
-                <div className='w-1/2 '>
-                    <h1 className='text-6xl font-bold text-green-100 leading-[75px]'>Amche Samaj Che <span className='text-orange-600'>Matrimony</span></h1>
+            <div className='grid md:flex gap-8 md:gap-0 items-center pt-16 sm:pt-20'>
+                <div className='md:w-1/2 '>
+                    <h1 className='text-5xl sm:text-6xl font-bold text-green-100 leading-[70px] md:leading-[75px]'>Amche Samaj Che <span className='text-orange-600'>Matrimony</span></h1>
                     <p className='text-white text-xl my-4'>Now find matches based on your hobbies & interests</p>
-                    <button className='text-white text-lg border-2 border-orange-500 px-6 py-2 mt-10 rounded-full rounded-br-none hover:bg-opacity-95 transition-all duration-300 '>Register</button>
+                    <button className='text-white text-lg border-2 border-orange-500 px-6 py-2 mt-6 sm:mt-10 rounded-full rounded-br-none hover:bg-opacity-95 transition-all duration-300 '>Register</button>
                 </div>
-                <div className='w-1/2 flex justify-center'>
-                    <div className='bg-orange-500 h-[500px] w-[500px] rounded-full p-2'>
-                        <div className='bg-green-200 h-[500px] w-[500px] rounded-full'>
-                            <img src={heroImage} alt="hero" className='w-full h-[500px]' />
+                <div className='md:w-1/2 flex justify-center'>
+                    <div className='bg-orange-500 md:h-[500px] md:w-[500px] rounded-full p-2'>
+                        <div className='bg-green-200 md:h-[500px] md:w-[500px] rounded-full'>
+                            <img src={heroImage} alt="hero" className='w-full  h-full md:h-[500px]' />
                         </div>
                     </div>
                 </div>
